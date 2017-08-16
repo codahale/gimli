2a6902420b719883feb19e252546fd90d7c56834
Coda Hale <coda.hale@gmail.com>
2017-08-15 19:25:00 -0700

    Initial commit.

---
+++ src/main/java/com/codahale/gimli/Gimli.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codahale.gimli;
+
+public interface Gimli {
+
+  static void transform(int[] state) {
+    int x;
+    int y;
+    int z;
+    for (int round = 24; round > 0; round--) {
+      for (int column = 0; column < 4; column++) {
+        x = Integer.rotateLeft(state[column], 24);
+        y = Integer.rotateLeft(state[4 + column], 9);
+        z = state[8 + column];
+
+        state[8 + column] = x ^ (z << 1) ^ ((y & z) << 2);
+        state[4 + column] = y ^ x ^ ((x | z) << 1);
+        state[column] = z ^ y ^ ((x & y) << 3);
+
+      }
+
+      // small swap
+      if ((round % 4) == 0) {
+        x = state[0];
+        state[0] = state[1];
+        state[1] = x;
+        x = state[2];
+        state[2] = state[3];
+        state[3] = x;
+      }
+
+      //  big swap
+      if ((round % 4) == 2) {
+        x = state[0];
+        state[0] = state[2];
+        state[2] = x;
+        x = state[1];
+        state[1] = state[3];
+        state[3] = x;
+      }
+
+      // add constant
+      if ((round % 4) == 0) {
+        state[0] ^= (0x9e377900 | round);
+      }
+    }
+  }
+}
---
+++ src/test/java/com/codahale/gimli/tests/GimliTest.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codahale.gimli.tests;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import com.codahale.gimli.Gimli;
+import org.junit.jupiter.api.Test;
+
+class GimliTest {
+
+  @Test
+  void testVector() {
+    final int[] x = new int[]{
+        0x00000000, 0x9e3779ba, 0x3c6ef37a, 0xdaa66d46,
+        0x78dde724, 0x1715611a, 0xb54cdb2e, 0x53845566,
+        0xf1bbcfc8, 0x8ff34a5a, 0x2e2ac522, 0xcc624026
+    };
+
+    Gimli.transform(x);
+
+    assertArrayEquals(new int[]{
+        0xba11c85a, 0x91bad119, 0x380ce880, 0xd24c2c68,
+        0x3eceffea, 0x277a921c, 0x4f73a0bd, 0xda5a9cd8,
+        0x84b673f0, 0x34e52ff7, 0x9e2bef49, 0xf41bb8d6
+    }, x);
+  }
+}
