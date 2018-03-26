147a9fc7c9b77a11b53466521a241cc63a563741
Coda Hale <coda.hale@gmail.com>
2018-03-26 16:11:44 -0700

    Switch to JUnit 5.

--- src/test/java/com/codahale/gimli/tests/GimliTest.java
+++ src/test/java/com/codahale/gimli/tests/GimliTest.java
@@ -15,15 +15,15 @@
  */
 package com.codahale.gimli.tests;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.codahale.gimli.Gimli;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class GimliTest {
+class GimliTest {
 
   @Test
-  public void testVector() {
+  void testVector() {
     final int[] x =
         new int[] {
           0x00000000, 0x9e3779ba, 0x3c6ef37a, 0xdaa66d46,
@@ -33,12 +33,19 @@ public class GimliTest {
 
     Gimli.permute(x);
 
-    assertArrayEquals(
-        new int[] {
-          0xba11c85a, 0x91bad119, 0x380ce880, 0xd24c2c68,
-          0x3eceffea, 0x277a921c, 0x4f73a0bd, 0xda5a9cd8,
-          0x84b673f0, 0x34e52ff7, 0x9e2bef49, 0xf41bb8d6
-        },
-        x);
+    assertThat(x)
+        .containsExactly(
+            0xba11c85a,
+            0x91bad119,
+            0x380ce880,
+            0xd24c2c68,
+            0x3eceffea,
+            0x277a921c,
+            0x4f73a0bd,
+            0xda5a9cd8,
+            0x84b673f0,
+            0x34e52ff7,
+            0x9e2bef49,
+            0xf41bb8d6);
   }
 }
